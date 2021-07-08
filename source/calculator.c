b6ba87189ff9e2ccd3589a841fff59c3371651aa
Willian-code <willianrosendodelima@outlook.com>
2021-07-08 17:45:35 -0300

    calculator

---
+++ source/calculator.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define KEY "Enter the calculator Operation you want to do:"
+
+// Function prototype declaration
+void addition();
+void subraction();
+void multiplication();
+void division();
+void modulus();
+void power();
+int factorial();
+int calculator_operations();
+
+// Start of Main Program
+int main()
+{
+    int x = 1;
+    char Calc_oprm;
+
+    // Function call
+    calculator_operations();
+
+    while(x)
+}
